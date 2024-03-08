e5038cedcf174d59e712c5cf5bb36e0b34ff6ca2
KieuTuanPhuong <maingmame23114@gmail.com>
2024-03-08 22:48:16 +0700

    update import date-fns

--- qr_checkin_employee/src/views/VacationRequest/VacationRequest.jsx
+++ qr_checkin_employee/src/views/VacationRequest/VacationRequest.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import axios from "axios";
-import { format } from "date-fns/format";
+import { format } from "date-fns";
 
 import { AuthContext } from "../../context/AuthContext";
 import Navigation from "../../components/Navigation/Navigation";
